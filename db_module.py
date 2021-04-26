915c0c000dc85b413b86386cb14cb0e63e0d4fed
Vitalygorkov <vet55omsk@gmail.com>
2021-04-26 23:57:44 +0600

    file db_module.py created

--- db_module.py
+++ db_module.py
@@ -7,8 +7,22 @@ conn.execute('''CREATE TABLE IF NOT EXISTS assets(
         assetsid INT,
         name TEXT PRIMARY KEY,
         descr TEXT,
+        price_1_day_year INT,
         price INT,
         change INT);''')
 print("Table created successfully")
 
+# conn.close()
+
+assetsid = '1'
+name = 'BTC'
+descr = 'its description'
+price_1_day_year = 28951
+price = 54200
+change = price/price_1_day_year*100
+
+
+assets = (assetsid, name, descr, price_1_day_year, price, change)
+conn.execute("INSERT INTO assets VALUES(?, ?, ?, ?, ?, ?);", assets)
+conn.commit()
 conn.close()
